0891d2da664afbf0a7f78689e644118a6d0c92ed
Chaewon <fromnowwon@gmail.com>
2024-02-07 13:55:12 +0900

    Handling Functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import List from "./components/List";
+import AddToList from "./components/AddToList";
 
-interface IState {
+export interface IState {
 	people: {
 		name: string;
 		age: number;
@@ -12,12 +13,32 @@ interface IState {
 }
 
 function App() {
-	const [people, setPeople] = useState<IState["people"]>([]);
+	const [people, setPeople] = useState<IState["people"]>([
+		{
+			name: "James",
+			url: "https://picsum.photos/id/24/100",
+			age: 36,
+			note: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+		},
+		{
+			name: "Kobe",
+			url: "https://picsum.photos/id/342/100",
+			age: 42,
+			note: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+		},
+		{
+			name: "Stephen",
+			url: "https://picsum.photos/id/4/100",
+			age: 32,
+			note: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+		},
+	]);
 
 	return (
 		<div className="App">
 			<h1>People Invited to my party</h1>
 			<List people={people} />
+			<AddToList people={people} setPeople={setPeople} />
 		</div>
 	);
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,13 @@
-interface IProps {
-	people: {
-		name: string;
-		age: number;
-		url: string;
-		note?: string;
-	}[];
-}
+import { IState as IProps } from '../App';
+
+// interface IProps {
+// 	people: {
+// 		name: string;
+// 		age: number;
+// 		url: string;
+// 		note?: string;
+// 	}[];
+// }
 
 // props에 직접 타입을 지정하는 방식
 // typescript는 JSX.Element 반환하는 함수로 인식
@@ -16,7 +18,26 @@ interface IProps {
 // 컴포넌트 명시
 // typescript는 함수형 컴포넌트를 반환하는 함수로 인식
 const List: React.FC<IProps> = ({ people }) => {
-	return <div>List</div>;
+
+	// JSX 엘리먼트 배열 반환 명시
+	const renderList = (): JSX.Element[] => {
+		return people.map((person, idx) => (
+			<li className="list" key={idx}>
+				<div className="list-header">
+					<img src={person.url} alt="" />
+					<h2>{person.name}</h2>
+				</div>
+				<p>{person.age} years old</p>
+				<p className="list-note">{person.note}</p>
+			</li>
+		))
+	}
+
+	return (
+		<ul>
+			{renderList()}
+		</ul>
+	)
 };
 
 export default List;
